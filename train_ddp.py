48e84a74979b505b715a1c2fb30084ffb950eb08
yeshwanth95 <staryesh@gmail.com>
2025-01-08 12:27:37 +0200

    added mass roads util functions
    added utility functions for mass roads dataset in ddp_utils.py and train_ddp.py

--- train_ddp.py
+++ train_ddp.py
@@ -19,9 +19,9 @@ from utils import (
 from ddp_utils import (
     get_inria_loaders,
     get_crowdai_loaders,
-    get_spacenet_loaders
     get_spacenet_loaders,
     get_whu_buildings_loaders,
+    get_mass_roads_loaders,
 )
 
 from models.model import (
@@ -164,6 +164,21 @@ def main():
         )
     elif "whu_buildings" in CFG.DATASET:
         train_loader, val_loader, _ = get_whu_buildings_loaders(
+            CFG.TRAIN_DATASET_DIR,
+            CFG.VAL_DATASET_DIR,
+            CFG.TEST_IMAGES_DIR,
+            tokenizer,
+            CFG.MAX_LEN,
+            tokenizer.PAD_code,
+            CFG.SHUFFLE_TOKENS,
+            CFG.BATCH_SIZE,
+            train_transforms,
+            valid_transforms,
+            CFG.NUM_WORKERS,
+            CFG.PIN_MEMORY,
+        )
+    elif "mass_roads" in CFG.DATASET:
+        train_loader, val_loader, test_loader = get_mass_roads_loaders(
             CFG.TRAIN_DATASET_DIR,
             CFG.VAL_DATASET_DIR,
             CFG.TEST_IMAGES_DIR,
