a3554c8c5aa204a8abb7255fa8afcce2f473c5c2
yeshwanth95 <staryesh@gmail.com>
2025-01-09 16:44:02 +0200

    updated mass_roads_clip_shapefile.py
    Enabled multiprocessing for clipping shapefiles. Greatly reduces processing time.

--- data_preprocess/mass_roads_clip_shapefile.py
+++ data_preprocess/mass_roads_clip_shapefile.py
@@ -2,10 +2,14 @@ from osgeo import gdal
 import os
 import subprocess
 from tqdm import tqdm
+from multiprocessing import Pool
 
 
-def clip_shapefile(raster_path, vector_path, save_path):
+def clip_shapefile(paths_dict):
     # get the extent of the raster
+    raster_path = paths_dict['raster_path']
+    save_path = paths_dict['save_path']
+    vector_path = paths_dict['vector_path']
     src = gdal.Open(raster_path)
     ulx, xres, xskew, uly, yskew, yres = src.GetGeoTransform()
     sizeX = src.RasterXSize * xres
@@ -23,6 +27,7 @@ def clip_shapefile(raster_path, vector_path, save_path):
 
     # call the command
     subprocess.call(cmd, shell=True)
+    return 0
 
 
 def main():
@@ -35,11 +40,23 @@ def main():
     rasters_dir = os.path.join(data_root, split, "sat")
     rasters = os.listdir(rasters_dir)
 
-    for raster in tqdm(rasters):
+    param_inputs = []
+    for raster in rasters:
         raster_path = os.path.join(rasters_dir, raster)
         save_path = os.path.join(save_dir, raster.split('.')[0]+".geojson")
-        clip_shapefile(raster_path, vector_path, save_path)
+        param_inputs.append(
+            {
+                'raster_path': raster_path,
+                'vector_path': vector_path,
+                'save_path': save_path,
+            }
+        )
+        # clip_shapefile(raster_path, vector_path, save_path)
+
+    with Pool() as p:
+        _ = list(tqdm(p.imap(clip_shapefile, param_inputs), total=len(param_inputs)))
 
 
 if __name__ == "__main__":
     main()
+
