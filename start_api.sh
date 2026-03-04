a38e92fa4231dc0bb2e427daae8977295374be6d
Jan Winkler <j.winkler@dirageosystems.ch>
2026-03-04 08:05:01 +0100

    Start api using uv run to use .venv

--- start_api.sh
+++ start_api.sh
@@ -5,4 +5,4 @@ set -e
 
 # Start the API server
 echo "Starting API server"
-uvicorn api:app --host 0.0.0.0 --port 8080 --workers 1 --backlog 10
+uv run uvicorn api:app --host 0.0.0.0 --port 8080 --workers 1 --backlog 10
